a0b81f69e3dda08ddc814bfdbc5cd7d224b347e8
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-02-28 09:17:43 +0000

    tp2 initial code with tp1 3 files

---
+++ tp2/MyTable.js
@@ -0,0 +1,45 @@
+/**
+* myTable
+* @param gl {WebGLRenderingContext}
+* @constructor
+*/
+
+class myTable extends CGFobject
+{
+  constructor(scene) {
+    super(scene);
+    this.quad=new MyUnitCubeQuad(this.scene);
+    this.leg=new myLeg(this.scene);
+    this.quad.initBuffers();
+    this.leg.initBuffers();
+  };
+
+  display(){
+    this.scene.pushMatrix();
+
+    this.scene.translate(2.35,1.75,1.35);
+    this.leg.display();
+
+    this.scene.translate(-4.7,0,0);
+    this.leg.display();
+
+    this.scene.translate(0,0,-2.7);
+    this.leg.display();
+
+    this.scene.translate(4.7,0,0);
+    this.leg.display();
+    
+    this.scene.popMatrix();
+    this.scene.pushMatrix();
+
+    
+    this.scene.translate(0,3.65,0);
+    this.scene.scale(5,0.3,3);
+
+    this.quad.display();
+  
+    this.scene.popMatrix();
+  }
+
+
+};
