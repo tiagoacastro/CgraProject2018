be354dc9fc16ef61b4d4129178635bcf6c82dac2
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-02-28 10:57:35 +0000

    mi chupi

--- tp2/tp2/LightingScene.js
+++ tp2/tp2/LightingScene.js
@@ -3,7 +3,7 @@ var degToRad = Math.PI / 180.0;
 var BOARD_WIDTH = 6.0;
 var BOARD_HEIGHT = 4.0;
 
-var BOARD_A_DIVISIONS = 1;
+var BOARD_A_DIVISIONS = 30;
 var BOARD_B_DIVISIONS = 100;
 
 class LightingScene extends CGFscene 
@@ -43,8 +43,8 @@ class LightingScene extends CGFscene
 		this.materialA = new CGFappearance(this);
 		this.materialA.setAmbient(0.3,0.3,0.3,1);
 		this.materialA.setDiffuse(0.6,0.6,0.6,1);
-		this.materialA.setSpecular(0.2,0.2,0.2,1);
-		this.materialA.setShininess(10);
+		this.materialA.setSpecular(0,0.2,0.8,1);
+		this.materialA.setShininess(120);
 
 		this.materialB = new CGFappearance(this);
 		this.materialB.setAmbient(0.3,0.3,0.3,1);
@@ -61,7 +61,7 @@ class LightingScene extends CGFscene
 
 	initLights() 
 	{
-		this.setGlobalAmbientLight(0.5,0.5,0.5, 1.0);
+		this.setGlobalAmbientLight(0,0,0, 0);
 		
 		// Positions for four lights
 		this.lights[0].setPosition(4, 6, 1, 1);
@@ -69,19 +69,32 @@ class LightingScene extends CGFscene
 		
 		this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
 		this.lights[1].setVisible(true); // show marker on light position (different from enabled)
+		
+		this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
+		this.lights[2].setSpecular(1,1,1,1);
 
-		//this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
 		//this.lights[1].setVisible(true); // show marker on light position (different from enabled)
-		//this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
+		this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
 		//this.lights[1].setVisible(true); // show marker on light position (different from enabled)
 
 		this.lights[0].setAmbient(0, 0, 0, 1);
 		this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
 		this.lights[0].enable();
 
+		this.lights[0].setSpecular(1,1,0,0);
+
 		this.lights[1].setAmbient(0, 0, 0, 1);
 		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
-		//this.lights[1].enable();
+		this.lights[1].enable();
+
+
+		this.lights[2].setDiffuse(0,1,0,1);
+		this.lights[2].enable();
+
+		this.lights[3].setVisible(true);
+		this.lights[3].setAmbient(0, 0, 0, 1);
+		this.lights[3].setDiffuse(0, 1, 0.2, 1.0);
+		this.lights[3].enable();
 	};
 
 	updateLights() 
