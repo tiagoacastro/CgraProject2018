55b9f7c413ebd7a0a09f20661f2b0a06776ad85e
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-16 11:21:03 +0100

    agr a dar oh bro

--- Projeto/src/MyVehicle.js
+++ Projeto/src/MyVehicle.js
@@ -11,16 +11,13 @@ class MyVehicle extends CGFobject
 		this.glass = new MyQuad(scene);
 
 		this.bodyAppearance = new CGFappearance(this.scene);
-   		this.bodyAppearance.loadTexture("../resources/images/stoneBrickMinecraft.png");
+   	this.bodyAppearance.loadTexture("../resources/images/stoneBrickMinecraft.png");
 		this.bodyAppearance.setTextureWrap("REPEAT", "REPEAT");
-    	this.bodyAppearance.setAmbient(0.3,0.3,0.3,1);
+		this.bodyAppearance.setAmbient(0.3,0.3,0.3,1);
 		this.bodyAppearance.setDiffuse(0.6,0.6,0.6,1);
 		this.bodyAppearance.setSpecular(0,0.2,0.8,1);
 		this.bodyAppearance.setShininess(120);
 
-		this.x = 0;
-		this.y =0;
-		this.z = 0;
 		this.glassAppearance = new CGFappearance(this.scene);
     this.glassAppearance.loadTexture("../resources/images/glass.png");
 		this.glassAppearance.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
@@ -29,21 +26,24 @@ class MyVehicle extends CGFobject
 		this.glassAppearance.setSpecular(0,0.2,0.8,1);
 		this.glassAppearance.setShininess(120);
 
-		this.initBuffers();
+		this.x = 0;
+		this.y =0;
+		this.z = 0;
 	};
-	
+
 	update (speed, forward) {
-	
+
 	if(forward)
 	this.z = this.z + speed;
 	else (this.z = this.z- speed);
-	
+
 	};
 
 	display() {
-		
+
 		this.scene.pushMatrix();
 		this.scene.translate(this.x,this.y,this.z);
+		
 		this.scene.pushMatrix();
     	this.scene.scale(2.5,1.5,4);
 		this.bodyAppearance.apply();
@@ -73,6 +73,8 @@ class MyVehicle extends CGFobject
 		 this.scene.rotate(1.57,0,1,0);
     this.wheel.display();
     this.scene.popMatrix();
+
+		this.scene.popMatrix();
 
 	}
 }
