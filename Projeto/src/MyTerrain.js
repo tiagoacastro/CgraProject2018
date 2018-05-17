3e846be90f5c914d881dd6dd6480b5525cb23565
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-17 16:04:22 +0100

    so mudei o myterrain, podes sacar as cenas

--- Projeto/src/MyTerrain.js
+++ Projeto/src/MyTerrain.js
@@ -2,7 +2,7 @@ class MyTerrain extends Plane
 {
 	constructor(scene)
 	{
-		super(scene,50,0,10,0,10);
+		super(scene,8,0,10,0,10);
 
 		this.terrainAppearance = new CGFappearance(this.scene);
     this.terrainAppearance.loadTexture("../resources/images/grassMinecraft.png");
@@ -13,12 +13,22 @@ class MyTerrain extends Plane
 		this.terrainAppearance.setSpecular(0,0.2,0.8,1);
 		this.terrainAppearance.setShininess(120);
 
-
-
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
 
+		this.altimetry= [[ 2.0 , 3.0 , 2.0, 4.0, 2.5, 2.4, 2.3, 1.3 ],
+		[ 2.0 , 3.0 , 2.0, 4.0, 7.5, 6.4, 4.3, 1.3 ],
+		[ 0.0 , 0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ],
+		[ 0.0 , 0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ],
+		[ 0.0 , 0.0 , 2.0, 4.0, 2.5, 2.4, 0.0, 0.0 ],
+		[ 0.0 , 0.0 , 2.0, 4.0, 3.5, 2.4, 0.0, 0.0 ],
+		[ 0.0 , 0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ],
+		[ 0.0 , 0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ],
+		[ 2.0 , 3.0 , 2.0, 1.0, 2.5, 2.4, 2.3, 1.3 ]
+		];
+
+		this.applyAltimetry();
 	};
 
 	display() {
@@ -28,5 +38,16 @@ class MyTerrain extends Plane
 		this.scene.translate(this.x, this.y, this.z);
 		super.display();
 		this.scene.popMatrix();
+	}
+
+	applyAltimetry(){
+		let c = 2;
+		for (var i = 0; i < this.altimetry.length; i++) {
+			for (var j = 0; j < this.altimetry[i].length; j++) {
+				this.vertices[c] = this.altimetry[i][j];
+				c+=3;
+			}
+		}
+		super.initGLBuffers();
 	}
 }
